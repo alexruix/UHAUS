58cdbc58b214b00bc09cf4d9707dc95d0d644aa4
alexruix <ruizgelp@gmail.com>
2024-10-08 23:59:19 -0300

    Animated Bug

--- src/layouts/script/animated.js
+++ src/layouts/script/animated.js
@@ -1,7 +1,6 @@
 const animateCSS = (element, animation, prefix = 'animate__') =>
     new Promise((resolve, reject) => {
       const animationName = `${prefix}${animation}`;
-      // Verificar si 'element' es un selector o un elemento DOM
       const node = typeof element === 'string' ? document.querySelector(element) : element;
   
       if (!node) {
@@ -24,13 +23,12 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
   const startPulseAnimation = async (elementSelector) => {
     while (true) {
       await animateCSS(elementSelector, 'pulse');
-      await new Promise(resolve => setTimeout(resolve, 4000)); // Esperar 4 segundos antes de la siguiente animación
+      await new Promise(resolve => setTimeout(resolve, 4000)); 
     }
   };
   
-  // Función para iniciar la animación fadeInUp con un retraso
   const startFadeInUpAnimation = async (elementSelector, delay) => {
-    await new Promise(resolve => setTimeout(resolve, delay)); // Esperar el delay antes de iniciar la animación
+    await new Promise(resolve => setTimeout(resolve, delay)); 
     await animateCSS(elementSelector, 'fadeInUp');
   };
   
@@ -43,40 +41,40 @@ const animateCSS = (element, animation, prefix = 'animate__') =>
 
 
   
-    // Observer para las tarjetas (.mycard)
-    const cards = document.querySelectorAll('.mycard'); // Seleccionamos todos los elementos con clase 'mycard'
+
+    const cards = document.querySelectorAll('.mycard'); 
     const observerCards = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          console.log('Elemento en pantalla:', entry.target); // Verifica si el elemento está en pantalla
+          console.log('Elemento en pantalla:', entry.target); 
           animateCSS(entry.target, 'flipInX');
           observer.unobserve(entry.target);
         }
       });
     }, {
-      threshold: 0.1 // El 10% del elemento debe estar visible para que se dispare la animación
+      threshold: 0.1 
     });
   
-    // Observamos cada card
+   
     cards.forEach(card => {
       observerCards.observe(card);
     });
   
-    // Observer para la clase .cta con animación fadeIn y un delay de 200ms
-    const ctaElement = document.querySelector('.cta'); // Seleccionamos el elemento con clase 'cta'
+    
+    const ctaElement = document.querySelector('.cta'); 
     const observerCTA = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          console.log('CTA en pantalla:', entry.target); // Verifica si el CTA está en pantalla
-          animateCSS(entry.target, 'fadeIn'); // Aplicar la animación fadeInUp con un delay de 200ms
-          observer.unobserve(entry.target); // Dejamos de observar una vez que se ha animado
+          console.log('CTA en pantalla:', entry.target);
+          animateCSS(entry.target, 'fadeIn'); 
+          observer.unobserve(entry.target); 
         }
       });
     }, {
-      threshold: 0.04 // El 10% del elemento debe estar visible para que se dispare la animación
+      threshold: 0.04 
     });
   
-    // Observar el elemento CTA
+    
     observerCTA.observe(ctaElement);
   });
   
